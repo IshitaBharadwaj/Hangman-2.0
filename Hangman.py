f44befe18bb3ee0c454f575c9bb9a3094253aed6
IshitaBharadwaj <68071562+IshitaBharadwaj@users.noreply.github.com>
2020-12-09 12:35:14 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -7,7 +7,7 @@ import random
     txt_file.close()
     return word2
 '''
-
+already= []
 def main():
 
     flag=0
@@ -22,33 +22,51 @@ def main():
         lines = [line.strip() for line in file]
         #lines = open(txt_file).read().splitlines()
         word= random.choice(lines).lower()
-        print(word)
-        word1= [0]*len(word)
+        #print(word)
+        word1= ['_']*len(word)
+        word_og=['_']*len(word)
+        for i in range(len(word)):
+            word_og[i]=word[i]
         print("Guess the word! :")
-        print(word1)
+        for i in range(len(word1)):
+            print(word1[i] + " ", end=" ")
+        print()
         tries=7
         while True:
             x= input("Enter an alphabet:")
-            flag=0
-            for j in range(len(word)):
-                if word[j]== x:
-                    word1[j]=x
-                    flag=1
-                else:
-                    continue
-            if flag==0:
-                tries-=1
-                print("You have " + str(tries) + " tries left!")
+            if x in already:
+                print("You have already entered this alphabet!")
+                continue
+            else:
+                already.append(x)
+                flag=0
+                for j in range(len(word)):
+                    if word[j]== x:
+                        word1[j]=x
+                        flag=1
+                    else:
+                        continue
+                if flag==0:
+                    tries-=1
+                    print("You have " + str(tries) + " tries left!")
+                    for i in range(len(word1)):
+                        print(word1[i]+ " ",end=" ")
+                    print()
 
-            if tries == 0:
-                print("Sorry, you lost and Game Over! ")
-                print("The word is: "+ word.upper())
-                break
+                if tries == 0:
+                    print("Sorry, you lost and Game Over! ")
+                    print("The word is: "+ word.upper())
+                    break
 
-            if flag ==1:
-                print(word1)
-            if tries!=0 and word1==word:
-                print("CONGRATULATIONS!")
+                if flag ==1:
+                    for i in range(len(word1)):
+                        print(word1[i]+ " ",end=" ")
+                    print()
+                if tries!=0 and word1==word_og:
+                    print()
+                    print("CONGRATULATIONS!")
+                    print("Game Over!!! You won!!!")
+                    break
 
 if __name__ == "__main__":
     main()
